6b4d6ccfd4eaad2290bde3a9d73d11c7fab83c2b
ZeroSinisi <zero.sinisi@gmail.com>
2018-03-30 00:45:44 -0400

    Creates server and client folders. Adds dependency on simple-git. Server send git status to the client.

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const git = require("simple-git")();
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = "8080";
+
+async function gitStatus() {
+    return new Promise(resolve => {
+        git.status((error, info) => {
+            resolve(info);
+        });
+    });
+}
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    gitStatus().then(status => {
+        res.end(JSON.stringify(status));
+    })
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
