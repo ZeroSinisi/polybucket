963770244bc1eef6e004c83606ed6b68cd29b8c8
ZeroSinisi <zero.sinisi@gmail.com>
2018-03-30 19:12:30 -0400

    Adds git status that prints to the console.

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const git = require("simple-git")();
 const http = require("http");
 
 const hostname = "127.0.0.1";
-const port = "8080";
+const port = "8081";
 
 async function gitStatus() {
     return new Promise(resolve => {
@@ -18,12 +18,34 @@ async function gitClone(repoPath, localPath, options, then) {
     });
 }
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
+function handleStatus(req, res) {
     gitStatus().then(status => {
+        res.statusCode = 400;
+        res.setHeader("Access-Control-Allow-Origin", "*");
         res.end(JSON.stringify(status));
+    }, error => {
+        console.log(error);
     })
+}
+
+function handleGet(req, res) {
+    switch (req.url) {
+        case "/status":
+            handleStatus(req, res);
+            break;
+    }
+}
+
+const server = http.createServer((req, res) => {
+    console.log("Request received");
+    switch (req.method) {
+        case "GET":
+            handleGet(req, res);
+            break;
+        case "POST":
+            console.log("Request was a POST");
+            break;
+    }
 });
 
 server.listen(port, hostname, () => {
